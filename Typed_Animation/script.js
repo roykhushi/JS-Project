1b2fa4d9f5d416bd3c4b3d89cb4164aa330d40e9
Khushi Roy <roykhushi432@gmail.com>
2024-03-07 23:51:19 +0530

    files added

---
+++ Typed_Animation/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    strings: ['Frontend Developer', 'Designer' , 'Student'], //xontent to show in the animation
+    typeSpeed: 100, //typing speed in ms
+    backSpeed: 100, //backspeed in ms
+    loop:true // infinite loop of the animation
+  });
